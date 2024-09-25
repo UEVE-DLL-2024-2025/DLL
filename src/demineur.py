a2be5874c26ae7abaaad3e312c0516689e32d22d
Sebb955 <slafrizi@gmail.com>
2024-09-25 14:14:44 +0200

    Update

--- src/demineur.py
+++ src/demineur.py
@@ -72,7 +72,7 @@ class Demineur:
         # Demande si le joueur souhaite recommencer une partie
         restart = input("Voulez-vous recommencer une partie ? (oui/non) : ").lower()
         if restart == 'oui':
-            self.__init__(self.nombre_mines)
+            self.nombre_mines
             self.jouer()
         else:
             print("Partie terminée !")
